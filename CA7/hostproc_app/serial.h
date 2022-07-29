0e4f602fc4cc5a1bd40969d0e85ab8418e055650
Iacopo <iacopo@almer-technologies.com>
2022-07-29 19:25:04 +0200

    structural changes

---
+++ CA7/hostproc_app/serial.h
@@ -0,0 +1,11 @@
+
+
+#ifndef HOSTPROC_APP_SERIAL_H
+#define HOSTPROC_APP_SERIAL_H
+
+typedef struct serial_dev serial_dev_t;
+
+int serial_setup(serial_dev_t * serial);
+
+
+#endif //HOSTPROC_APP_SERIAL_H
