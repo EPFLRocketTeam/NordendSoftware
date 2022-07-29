7378fa60266f64c83fc7ebd7fcea4f6d49f67320
Iacopo <iacopo@almer-technologies.com>
2022-07-29 19:25:15 +0200

    structural changes

---
+++ CA7/hostproc_app/main.c
@@ -0,0 +1,15 @@
+// C library headers
+#include <stdio.h>
+#include <string.h>
+
+// Linux headers
+#include <fcntl.h>
+#include <errno.h>
+#include <termios.h>
+#include <unistd.h>
+
+int main(void) {
+
+
+
+}
