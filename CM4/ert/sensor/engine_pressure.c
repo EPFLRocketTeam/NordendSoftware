55eeb3c7c8b79fe66fe55fbb9f6128207f5e20c9
ThiloChalas <thilo.chalas@epfl.ch>
2022-11-17 14:16:53 +0100

    Add new accelerometer driver

---
+++ CM4/ert/sensor/engine_pressure.c
@@ -0,0 +1,7 @@
+/*  Title		: engine_pressure.c
+ *  Filename	: engine_pressure.c
+ *	Author		: Thilo Chalas, Luciano Calcoen
+ *	Date		: 17.11.2022
+ *	Version		: 0.1
+ *	Description	: Engine nozzle pressure sensor setup and acquisition
+ */
---
+++ CM4/ert/sensor/engine_pressure.h
@@ -0,0 +1,7 @@
+/*  Title		: engine_pressure.h
+ *  Filename	: engine_pressure.h
+ *	Author		: Thilo Chalas, Luciano Calcoen
+ *	Date		: 17.11.2022
+ *	Version		: 0.1
+ *	Description	: Engine nozzle pressure sensor setup and acquisition
+ */
