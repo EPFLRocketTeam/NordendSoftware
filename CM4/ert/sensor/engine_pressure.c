20d34fb56a1e90e85295f1bf63918f1efd4ab0ce
Sanso <luciano.calcoen@gmail.com>
2023-03-15 10:21:04 +0100

    Fixed the formula

--- CM4/ert/sensor/engine_pressure.c
+++ CM4/ert/sensor/engine_pressure.c
@@ -18,33 +18,28 @@
 
 #define PRESS_SENS_ADC_CHANNEL 1
 
-//in MPa
+//in bar
 #define MIN_PRESSURE 0
 #define MAX_PRESSURE 10
 
-//in mA
-#define MIN_CURRENT 4
-#define MAX_CURRENT 20
-
-//in Ohm
-#define DIFFERENTIAL_RESISTOR 100
+//in V (must be defined with the type of sensor)
+#define AA_Vdc 5
+#define CH_Vdc 8
 
 //static double const VREF = 2.048;
-static double LIN_RATIO = 0;
+const static double LIN_RATIO = (MAX_PRESSURE-MIN_PRESSURE)/(0.8*AA_Vdc);
 
 void engine_sensor_convert_pres(double * data);
 
 util_error_t engine_pressure_init(device_t * eng_press){
     util_error_t error = ER_SUCCESS;
     error |= adc_init(eng_press);
-    LIN_RATIO = (double)((MAX_PRESSURE-MIN_PRESSURE)/(
-        (DIFFERENTIAL_RESISTOR*MAX_CURRENT)-(DIFFERENTIAL_RESISTOR*MIN_CURRENT)));
     return error;
 }
 
 /*to-do once we have the hardware values (Vref, Vmin, Vmax)*/
 void engine_sensor_convert_pres(double * data) {
-	*data = LIN_RATIO * (*data);
+	*data = LIN_RATIO * (*data - 0.1*AA_Vdc) + MIN_PRESSURE;
 }
 
 util_error_t engine_pressure_read(device_t * eng_press, double * data) {
@@ -56,6 +51,6 @@ util_error_t engine_pressure_read(device_t * eng_press, double * data) {
 
 util_error_t engine_pressure_calibrate(device_t * eng_press) {
     util_error_t error = ER_SUCCESS;
-    //TODO: write some sort of calibration code for the sensor
+    //no calibration necessary as the pressure_sensor "will" stay in the range of Pressure min and pressure max 
     return error;
 }
