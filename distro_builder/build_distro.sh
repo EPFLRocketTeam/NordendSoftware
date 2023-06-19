d8b452ea0e0522e63f349d69e80a1447f71469df
Iacopo <iacopo.sprenger@epfl.ch>
2023-06-19 12:55:06 +0200

    Added docker builder, to compile the distribution on any system.

---
+++ distro_builder/build_distro.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ../CA7/DeviceTree/
+patch -p3 --forward --no-backup-if-mismatch < 0001-add-uart-bias.patch
+cd -
+
+cd ../distribution-package
+
+MACHINE=stm32mp1-wildhornav DISTRO=openstlinux-ert EULA_stm32mp1wildhornav=1 source layers/meta-st/scripts/envsetup.sh --no-ui
+
+bitbake ert-image-core
+
+echo "done!"
